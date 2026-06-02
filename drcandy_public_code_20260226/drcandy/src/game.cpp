c07e7f1681145a7b9779d2198e664d1615eca588
1798565 <1798565@uab.cat>
2026-06-02 11:46:40 +0200

    Afegir gitignore i treure carpeta build

--- drcandy_public_code_20260226/drcandy/src/game.cpp
+++ drcandy_public_code_20260226/drcandy/src/game.cpp
@@ -72,6 +72,8 @@ void Game::update(const Controller& controller)
 void Game::render(GraphicManager& graphics)
 {
     // Implement your code here
+    //Començo
+    
     
     // Note: the following code exhibits the main graphic library features
     // Board: border [draw rectangles] and a single piece of candy
