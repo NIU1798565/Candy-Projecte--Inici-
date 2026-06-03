1953a283ebcf613ff01c7399e55ccdc2bd080215
1798565 <1798565@uab.cat>
2026-06-03 23:23:43 +0200

    correccio argument main

--- drcandy_public_code_20260226/drcandy/src/main.cpp
+++ drcandy_public_code_20260226/drcandy/src/main.cpp
@@ -16,6 +16,10 @@ int main(int argc, const char* argv[])
 
     // The following code runs the graphic part
     Game game;
+    if (argc > 1)
+    {
+        game.load(argv[1]);
+    }
     // Run until ESC is pressed
     game.run();
     return 0;
